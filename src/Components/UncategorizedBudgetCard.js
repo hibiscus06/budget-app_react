ff73876ae6eaaf5f104befe4751d5ee39bb87b71
Rudrika Singh <72783505+hibiscus06@users.noreply.github.com>
2024-02-22 17:54:07 +0530

    Update UncategorizedBudgetCard.js

--- src/Components/UncategorizedBudgetCard.js
+++ src/Components/UncategorizedBudgetCard.js
@@ -1,14 +1,13 @@
-import useBudgets from '../contexts/BudgetContext'
-import UNCATEGORIZED_BUDGET_ID from "../contexts/BudgetContext"
-import BudgetCard from './BudgetCard'
+import useBudgets, { UNCATEGORIZED_BUDGET_ID } from "../contexts/BudgetContext"
+import BudgetCard from "./BudgetCard"
 
 export default function UncategorizedBudgetCard(props) {
+  const { getBudgetExpenses } = useBudgets()
+  const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce(
+    (total, expense) => total + expense.amount,
+    0
+  )
+  if (amount === 0) return null
 
-    const { getBudgetExpenses } = useBudgets()
-    const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce(
-        (total, expense) => total + expense.amount , 0)
-
-        //if (amount === 0) return null
-    return <BudgetCard name="Uncategorized" amount={amount} gray {...props} />
-    
+  return <BudgetCard amount={amount} name="Uncategorized" gray {...props} />
 }
